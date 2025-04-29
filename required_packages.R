ba9907be1a175ee437efbeed939c3eba2fcdb403
Jyoti <82015548+Jyoti-Jittu@users.noreply.github.com>
2025-04-29 13:10:44 +0530

    Add files via upload

---
+++ required_packages.R
@@ -0,0 +1,11 @@
+set.seed(100)
+
+# Install required packages
+install.packages("e1071")
+install.packages("MendelianRandomization")
+install.packages("caret")
+install.packages("dplyr")
+install.packages("meta")
+install.packages("ggplot2")
+install.packages("remotes")
+remotes::install_github("MRCIEU/TwoSampleMR")
